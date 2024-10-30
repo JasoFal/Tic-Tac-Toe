548f3e2045ad43dc9910a85805d6bbe4f1d5a986
Jason Falk <jasonfalk9@gmail.com>
2024-10-30 15:24:13 -0700

    Add redux imports and functionality

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 import App from "./components/App"
+import { createStore } from "redux";
+import reducer from "./reducers/game_reducer";
+import { Provider } from "react-redux";
+
+const store = createStore(reducer);
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
